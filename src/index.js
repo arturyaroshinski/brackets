a8fdfb296dec7eb3983afb5bc492029b2509bbca
Artur Yaroshinski <62154380+arturyaroshinski@users.noreply.github.com>
2021-03-03 21:50:36 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let cfg = bracketsConfig.map((x) => x.join(""));
+
+    for (let i = 0; i < cfg.length; ) {
+        if (str.includes(cfg[i])) {
+            str = str.split(cfg[i]).join("");
+            i = 0;
+            continue;
+        }
+
+        i++;
+    }
+
+    return str ? false : true;
+};
